517c3e161ae9fffffe3573d2f7bf85f73eae0933
VictorFreir <victorvfreire@gmail.com>
2024-09-15 12:06:51 -0300

    socket communication

--- main.go
+++ main.go
@@ -10,73 +10,110 @@ import (
 )
 
 const (
-	PORT       = 10000
 	PEERS_FILE = "peers.txt"
 )
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	fmt.Println("Conexão recebida de ", conn.RemoteAddr().String())
+type conn struct {
+	ip   string
+	port string
+	conn net.Conn
+}
 
-	fileHash, _ := bufio.NewReader(conn).ReadString('\n')
-	fileHash = strings.TrimSpace(fileHash)
+func handleConnection(conn net.Conn, peersCh chan net.Conn) {
+	fmt.Println("Conexão recebida de ", conn.RemoteAddr().String())
 
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
-		fmt.Printf("Mensagem recebida: %s\n", scanner.Text())
+		msg := scanner.Text()
+		fmt.Printf("Mensagem recebida: %s\n", msg)
+
+		response := "Recebido: " + msg
+		_, err := conn.Write([]byte(response + "\n"))
+		if err != nil {
+			log.Println("Erro ao enviar resposta:", err)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Println("Erro na leitura:", err)
 	}
+
+	peersCh <- conn
 }
 
-// registerIP
-func registerIP(address string) {
-	file, err := os.OpenFile(PEERS_FILE, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+func startServer(ip string, port string, peersCh chan net.Conn) {
+	selfAddress := ip + ":" + port
+	ln, err := net.Listen("tcp", selfAddress)
 	if err != nil {
-		log.Fatal("PEERS_FILE não pôde ser aberto.")
-	}
-	defer file.Close()
+		connectToPeer(ip, port, peersCh)
+	} else {
+		defer ln.Close()
 
-	_, err = file.WriteString(address + "\n")
-	if err != nil {
-		log.Fatal("erro ao escrever no arquivo: ", err)
-	}
-}
+		fmt.Printf("Servidor está ouvindo no endereço %s\n", selfAddress)
 
-func startServer(port int) {
-	selfAddress := GetLocalIP()
-	listenAddress := fmt.Sprintf("%s:%d", selfAddress, port)
-	ln, err := net.Listen("tcp", listenAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ln.Close()
-
-	registerIP(listenAddress)
-
-	fmt.Printf("Servidor está ouvindo no endereço %s\n", listenAddress)
-
-	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Println("Erro na aceitação de conexão:", err)
-			continue
+			return
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, peersCh)
 	}
 }
 
-func main() {
-
-	go startServer(PORT)
-
-	for {
-
+func connectToPeer(address string, port string, peersCh chan net.Conn) {
+	selfAddress := address + ":" + port
+	conn, err := net.Dial("tcp", selfAddress)
+	if err != nil {
+		log.Printf("Erro ao conectar ao peer %s: %v", address, err)
+		return
 	}
 
-	// pegar os pares e o file_hash
-	// se houver pares, executa a busca com file_hash e imprime as máquinas encontradas.
-	// senão, não imprime nada pois não há pares.
-	//
+	fmt.Println("Conectado ao peer:", address)
+
+	peersCh <- conn
+}
+
+func startAllConnections(peersCh chan net.Conn) {
+	file, err := os.Open(PEERS_FILE)
+	if err != nil {
+		log.Fatalf("Erro ao abrir o arquivo de peers: %v", err)
+	}
+	defer file.Close()
+
+	var peers []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		address := scanner.Text()
+		address = strings.TrimSpace(address)
+		if address != "" {
+			peers = append(peers, address)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Erro ao ler o arquivo de peers: %v", err)
+	}
+
+	for _, peer := range peers {
+		parts := strings.Split(peer, ":")
+		ip := parts[0]
+		port := parts[1]
+		go startServer(ip, port, peersCh)
+	}
+
+}
+
+func sendMessage(conn net.Conn, message string) {
+	_, err := conn.Write([]byte(message + "\n"))
+	if err != nil {
+		log.Printf("Erro ao enviar mensagem: %v", err)
+		return
+	}
+	fmt.Printf("Mensagem enviada para %s: %s\n", conn.RemoteAddr().String(), message)
+}
+
+func main() {
+	peersCh := make(chan net.Conn)
+	startAllConnections(peersCh)
+	a := <-peersCh
+	sendMessage(a, "oi")
 }
